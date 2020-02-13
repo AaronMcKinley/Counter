4d7148b87d246fe64766913ab87f93ecdb884f35
AaronMcKinley <aaron09912@gmail.com>
2020-02-13 12:47:18 +0000

    Tests done

--- src/main/java/ie/gmit/Counter.java
+++ src/main/java/ie/gmit/Counter.java
@@ -1,8 +1,28 @@
 package ie.gmit;
 
 public class Counter {
+    public int count;
 
+    public Counter(){
+        count=0;
+    }
+    public Counter(int aCount){
+        if(aCount>0) {
+            count = aCount;
+        }
+        else{
+            throw new IllegalArgumentException("Less than 0");
+        }
+    }
     public int getCount() {
-        return 0;
+        return count;
+    }
+    public int incrementCount(){
+        ++count;
+        return count;
+    }
+    public int decrementCount(){
+        --count;
+        return count;
     }
 }
--- src/test/java/ie/gmit/TestCounter.java
+++ src/test/java/ie/gmit/TestCounter.java
@@ -1,15 +1,32 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCounter {
 
-    Counter myCounter;
+    private Counter myCounter;
 
+    @BeforeEach
+    void setUpClass(){
+        myCounter = new Counter();
+    }
     @Test
     void testConstructor(){
-        myCounter = new Counter();
-        assertEquals(0, myCounter.getCount());
+        assertEquals(myCounter.count, myCounter.getCount());
+    }
+    @Test
+    void testIncrement(){
+        assertEquals(myCounter.incrementCount()+1, myCounter.incrementCount());
+    }
+    @Test
+    void testDecrement(){
+        assertEquals(myCounter.decrementCount()-1, myCounter.decrementCount());
+    }
+    @Test
+    void testGreaterThan(){
+        Counter aCounter = new Counter(3);
     }
 }
